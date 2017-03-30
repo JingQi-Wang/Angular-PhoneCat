6f6b5eb49d6bb87611b9c4b7b099c3275e13bd60
JingQi-Wang <15757856529@163.com>
2017-03-30 16:20:51 +0800

    建立仓库

---
+++ app/js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
+
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      when('/daye', {
+        templateUrl: 'partials/daye.html',
+        controller: 'dayeCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
